220e2fd2152ea3859999ebd37a1d8cbcf511498b
Bryan Lin and David Shin <david@>
2017-04-10 13:51:26 -0700

    add turn indicator function to scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,10 @@ function flipCard(card) {
   if (card.hasClass("ghost")) {
     if (player1.isTurn === true) {
       player1.choseGhost = true;
+      alert("YOU LOSE 1")
     } else {
       player2.choseGhost = true;
+      alert("YOU LOSE 2")
     }
   } else {
     if (player1.isTurn === true) {
@@ -25,11 +27,24 @@ function flipCard(card) {
   }
 }
 
+function playerTurn() {
+  if(player2.isTurn===true) {
+    $("#player1").removeClass("player1Turn");
+    $("#player2").addClass("player2Turn");
+  } else {
+    $("#player1").addClass("player1Turn");
+    $("#player2").removeClass("player2Turn");
+  }
+}
+
 $(document).ready(function() {
+  playerTurn();
   $(".col-md-3").click(function() {
     $(this).find(".front").show();
     $(this).find(".back").hide();
     var card = $(this);
     flipCard(card);
+    playerTurn();
+    console.log(player1);
   });
 });
