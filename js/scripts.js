def514f367e4221e3d37b5f638be6d6e9fa1802a
Max Reichman <reichmanmax@gmail.com>
2017-04-10 10:58:35 -0700

    Create player object constructor and objects. Add function for flipCard().

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function Player(name, isTurn, choseGhost) {
+  this.name = name;
+  this.isTurn = isTurn;
+  this.choseGhost = choseGhost;
+}
+
+var player1 = new Player(player1, true, false);
+var player2 = new Player(player2, false, false);
+
+function flipCard() {
+  if ($(this).hasClass("ghost")) {
+    if (player1.isTurn === true) {
+      player1.choseGhost = true;
+    } else {
+      player2.choseGhost = true;
+    }
+  } else {
+    if (player1.isTurn === true) {
+      player1.isTurn = false;
+      player2.isTurn = true;
+    } else {
+      player2.isTurn = false;
+      player1.isTurn = true;
+    }
+  }
+}
+
+$(document).ready(function() {
+  $(".col-md-3").click(function() {
+    flipCard();
+  });
+});
